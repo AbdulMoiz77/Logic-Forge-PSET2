38c7a41be98f25b4de6a51a05a9d8ecac9f68c26
AbdulMoiz77 <amoizmateen777@gmail.com>
2026-01-10 17:19:24 +0500

    Improved the Solution
    Removed the extra iteration

--- challenge1_mountain_peak.py
+++ challenge1_mountain_peak.py
@@ -1,14 +1,14 @@
-def mountainPeak(n):    
-    if n < 1 or n > 45:
+def mountainPeak(n):
+    if n < 1 or n > 45: # out of range (1 <= n <= 45)
         return 0  
 
     if n == 1:
         return 1
 
     # fibonacci series
-    a = 0
+    a = 1
     b = 1
-    for i in range(n):
+    for i in range(1, n):
         c = a + b
         a = b
         b = c
