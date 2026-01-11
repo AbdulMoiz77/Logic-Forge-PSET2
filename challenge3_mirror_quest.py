10a009b29f9aa213799a6eb6b7ed602a67fdee4d
AbdulMoiz77 <amoizmateen777@gmail.com>
2026-01-12 01:23:47 +0500

    Improved the solution

--- challenge3_mirror_quest.py
+++ challenge3_mirror_quest.py
@@ -1,26 +1,36 @@
-def find_longest_mirror_length(s, start, end):
-    if start > end:        
-        return 0
-    
-    if start == end:
-        return 1
-    
-    if s[start] == s[end]:
-        start = start + 1   # second character
-        end = end - 1       # second last character
-        return find_longest_mirror_length(s, start, end) + 2
-    else:
-        # throwing away the first character
-        length1 = find_longest_mirror_length(s, start+1, end)
-
-        # throwing away the last character
-        length2 = find_longest_mirror_length(s, start, end - 1)
-
-        if length1 > length2:
-            return length1
-        return length2
+def find_longest_mirror_length(s):
+    def mirror_length(start, end, d):
+        if (start, end) in d: #already found the length for (start, end) pair so returning it instead of finding again
+            return d[(start, end)]
+        
+        if start > end:        
+            return 0
+        
+        if start == end: # only one character left (middle character incase of odd length substring)
+            return 1
+        
+        if s[start] == s[end]: # first and last character matches    
+            length = mirror_length(start+1, end-1, d) + 2
+        else:
+            length = max( mirror_length(start+1, end, d), # thrwoing away the first character
+                         mirror_length(start, end - 1, d)) # throwing away the last character
+            
+        d[(start, end)] = length
+        return length
 
 
-st = "bbabcbcab"
-print(find_longest_mirror_length(st, 0, len(st) - 1))
+    return mirror_length(0, len(s)-1, {})
+
+if __name__ == "__main__":
+    st = "bbabcbcab"
+    print(find_longest_mirror_length(st))
+
+    st = "GEEKS"
+    print(find_longest_mirror_length(st))
+
+    st = "MAPAM"
+    print(find_longest_mirror_length(st))
+
+    st = "ABCD"
+    print(find_longest_mirror_length(st))
     
